2a1d5e9f0f33a5a4dd116a24036d36d9865b82e8
Tom <tomhooijenga@gmail.com>
2026-05-06 21:47:24 +0200

    Stable clientId

--- src/ably.ts
+++ src/ably.ts
@@ -1,8 +1,10 @@
 import Ably from "ably";
 
+localStorage.setItem('uuid', localStorage.getItem('uuid') ?? crypto.randomUUID());
+
 const client = new Ably.Realtime({
   key: import.meta.env.VITE_ABLY_API_KEY as string,
-  clientId: localStorage.getItem('uuid') ?? crypto.randomUUID(),
+  clientId: localStorage.getItem('uuid'),
 });
 
 export default client;
