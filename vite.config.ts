76b2c512ab3097978eee278c027d5c1b028d32b4
Tom <tomhooijenga@gmail.com>
2026-05-07 23:14:27 +0200

    Netlify config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig(() => ({
   plugins: [react(), tailwindcss()],
-  base: command === 'build' ? 'scrumgif' : '/',
 }));
